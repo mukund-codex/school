34e356fdedfbc19127b2eac56dfae3f53c3757b5
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-06-02 15:03:24 +0530

    Updated Routes.

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,14 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('login-handler', [\App\Http\Controllers\UserController::class, 'loginHandler'])->name('login-handler');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
+
+
+//VLCCKabir@202401
+//07059939335

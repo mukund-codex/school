d021b95286119fc7b0ab7bbbd5221560531bb048
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-06-06 16:29:42 +0530

    Update code.

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
 });
 
-Route::post('login-handler', [\App\Http\Controllers\UserController::class, 'loginHandler'])->name('login-handler');
-
+Route::post('/login-handler', [UserController::class, 'loginHandler'])->name('login-handler');
+Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
